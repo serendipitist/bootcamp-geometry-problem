5f83f33b459b24cde3a138881b113712a76efa42
shireesha <shireesha@quintype.com>
2016-09-29 16:36:58 +0530

    Modify test spec & method to find length

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -2,8 +2,9 @@ require "spec_helper"
 
 describe Geometry::Point do
   describe "#point" do
-    it "should return the square of lenght for (0,0) and (0,2)"
-      point = Geometry::point.new(0,0,0,2)
+    it "should return length = 2  for (0,0) and (0,2)" do
+      point = Geometry::Point.new(0,0,0,2)
       expect(point.length_between_2_points).to eq(2)
+    end
   end
 end
