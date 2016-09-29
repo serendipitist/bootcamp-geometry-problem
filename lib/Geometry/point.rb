2683599373879436efc8080a11c4b5d0566c5985
shireesha <shireesha@quintype.com>
2016-09-29 16:24:00 +0530

    Add length_between_2_points method

--- lib/Geometry/point.rb
+++ lib/Geometry/point.rb
@@ -3,9 +3,15 @@ class Geometry::Point
  attr_reader :x_point, :y_point
 
   def initialize(x1,y2,x2,y2)
-    @x_point = x
-    @y_point = y
+    @x1_point = x1
+    @y1_point = y1
+    @x2_point = x2
+    @y2_point = y2
   end
+
+ def length_between_2_points
+   2
+ end
 
  private
   attr_writer :x_point ,:y_point
