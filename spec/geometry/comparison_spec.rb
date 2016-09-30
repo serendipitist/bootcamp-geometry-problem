85c9cbff13390be65e262ff1122a1e03ca0f4dcd
shireesha <shireesha@quintype.com>
2016-09-30 12:50:27 +0530

    Add test spec for checking reflexive property

--- spec/geometry/comparison_spec.rb
+++ spec/geometry/comparison_spec.rb
@@ -7,16 +7,21 @@ describe Geometry::Comparison do
     #   expect(compare.compare_points).to eq(true)
     # end
 
-    it 'should be true when both  abscissas and ordinates are ' do
+    it 'should be true when both  abscissas and ordinates  equal' do
       point1 = Geometry::Comparison.new(1, 2)
       point2 = Geometry::Comparison.new(1, 2)
-
       expect(point1 == point2).to eq(true)
     end
+
     it 'should be false when both  abscissas and ordinates are not equal' do
       point1 = Geometry::Comparison.new(1, 2)
       point2 = Geometry::Comparison.new(1, 3)
       expect(point1 == point2).to eq(false)
+    end
+
+    it 'should return true when both the abscissas and ordinates are same point(Reflexive property)'do
+      point1 = Geometry::Comparison.new(1, 1)
+      expect(point1 == point1).to eq(true)  
     end
   end
 end
