5452a449beada1191c59639cc898ac90dc85a652
shireesha <shireesha@quintype.com>
2016-10-01 16:44:40 +0530

    Test spec for new polygon for computing perimeter

--- lib/geometry/square.rb
+++ lib/geometry/square.rb
@@ -1,7 +1,7 @@
-class Geometry::Square < Geometry::Rectangle
-
-  def initialize(side)
-    super(side,side)
-
-  end
-end
+# class Geometry::Square < Geometry::Rectangle
+#
+#   def initialize(side)
+#     super(side,side)
+#
+#   end
+# end
