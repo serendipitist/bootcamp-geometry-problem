1a47678909e7a9176ee2cf407d000b7fac9dc20f
shireesha <shireesha@quintype.com>
2016-10-01 11:54:04 +0530

    Calculated for different abscissa and ordinate

--- spec/geometry/distance_spec.rb
+++ spec/geometry/distance_spec.rb
@@ -6,8 +6,9 @@ describe Geometry::Point do
       point1 = Geometry::Point.new(0, 0)
       point2 = Geometry::Point.new(0, 1)
       point3 =Geometry::Point.new(0,2)
+      point4 =Geometry::Point.new(1,1)
       expect(point1.distance_between_points(point2)).to eq(1)
-      expect(point2.distance_between_points(point3)).to eq(1)
+      expect(point1.distance_between_points(point4)).to eq(Math.sqrt(2))
     end
   end
 
