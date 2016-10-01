8c0bb839d96d53af2c676a38041ab16b09b8c81c
shireesha <shireesha@quintype.com>
2016-10-01 11:58:24 +0530

    added spec for float distance

--- spec/geometry/distance_spec.rb
+++ spec/geometry/distance_spec.rb
@@ -5,9 +5,10 @@ describe Geometry::Point do
     it 'should calculate the distance when given 2 points' do
       point1 = Geometry::Point.new(0, 0)
       point2 = Geometry::Point.new(0, 1)
-      point3 =Geometry::Point.new(0,2)
-      point4 =Geometry::Point.new(1,1)
-      expect(point1.distance_between_points(point2)).to eq(1)
+      point3 = Geometry::Point.new(0,2)
+      point4 = Geometry::Point.new(1,1)
+      expect(point1.distance_between_points(point2)).to eq(Math.sqrt(1))
+      expect(point1.distance_between_points(point3)).to eq(Math.sqrt(4))
       expect(point1.distance_between_points(point4)).to eq(Math.sqrt(2))
     end
   end
