2ff16f9d71de135f0281179691b7220f31920bd7
shireesha <shireesha@quintype.com>
2016-10-01 15:02:05 +0530

    To add spec for perimeter new class rectangle

--- lib/geometry/point.rb
+++ lib/geometry/point.rb
@@ -6,9 +6,8 @@ class Geometry::Point
     @ordinate = ordinate
   end
 
-  def distance_between_points(point2)
-    Math.sqrt((self.abscissa - point2.abscissa)**2 + (self.ordinate - point2.ordinate)**2)
+  def distance(other)
+    Math.sqrt((self.abscissa - other.abscissa)**2 + (self.ordinate - other.ordinate)**2)
   end
 
-  alias :eql? :==
 end
--- spec/geometry/distance_spec.rb
+++ spec/geometry/distance_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Geometry::Point do
-  describe '#distance_between_points?' do
+  describe '#distance' do
     it 'should calculate the distance when given 2 points' do
       point1 = Geometry::Point.new(0, 0)
       point2 = Geometry::Point.new(0, 1)
       point3 = Geometry::Point.new(0,2)
       point4 = Geometry::Point.new(1,1)
-      expect(point1.distance_between_points(point2)).to eq(Math.sqrt(1))
-      expect(point1.distance_between_points(point3)).to eq(Math.sqrt(4))
-      expect(point1.distance_between_points(point4)).to eq(Math.sqrt(2))
+      expect(point1.distance(point2)).to eq(Math.sqrt(1))
+      expect(point1.distance(point3)).to eq(Math.sqrt(4))
+      expect(point1.distance(point4)).to eq(Math.sqrt(2))
     end
   end
 
